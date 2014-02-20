56589f52e2f712e31f56eec0431b30f4fe5a8a5e
mattj24@gmail.com <mattj24@gmail.com@72710deb-69ad-d998-ebc1-45e36a55b302>
2014-02-20 06:20:55 +0000

    APIs added: register, login, verify New dependencies: bcryptjs

--- Backend/README.txt
+++ Backend/README.txt
@@ -2,7 +2,7 @@
 Setup:
 1) Download and install NodeJS from http://nodejs.org/download/
 2) Open a command prompt in the location of the project.
-3) Run the following command: npm install express mysql
+3) Run the following command: npm install express mysql bcryptjs
 
 Running on Windows:
 1) Use the command: node server.js
--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,13 @@
 var port = 8080;
-var express = require('express')
-var mysql = require('mysql')
+var express = require('express');
+var mysql = require('mysql');
+var bcrypt = require('bcryptjs');
+var crypto = require('crypto');
 var app = express();
 
+//Possibly use a connection pool instead.
 var db = mysql.createConnection({
-  host     : 'localhost',
+  host     : 'ec2-54-200-98-161.us-west-2.compute.amazonaws.com',
   database : 'app',
   user     : 'app',
   password : 'u5zwwmGh8MVzanp!'
@@ -13,6 +16,7 @@ var db = mysql.createConnection({
 db.connect();
 
 app.configure(function() {
+	//app.use(express.bodyParser())
     app.use(express.json());
     app.use(express.urlencoded());
 });
@@ -34,12 +38,55 @@ app.post('/login', function (request, response) {
 	//request.headers['authorization']
     //response.json({test:'test'});
 
-    var user = request.body.user;
+    var username = request.body.username;
     var password = request.body.password;
 
-    //To-do: hash password and check database
+    db.query("SELECT UserId, Password, Salt FROM Users WHERE UserName = ?", username, function(err, rows, fields) {
 
-    response.send(200, {key:'0'});
+    	if(err) {
+
+    		console.log(err);
+    		response.send(500, {error: "An error has occured."});
+
+    	} else {
+
+    		if(rows.length === 0) {
+    			response.send(500, {error: "Invalid username or password."});
+    		} else {
+    			bcrypt.hash(password, rows[0].Salt, function(err, hash) {
+
+    				if(err) {
+    					console.log(err);
+    					response.send(500, {error: "An error has occured."});
+    				} else {
+    					if(hash != rows[0].Password) {
+							response.send(500, {error: "Invalid username or password."});
+    					} else {
+			        		var key = generateSessionKey(username);
+			        		var session = {SessionKey: key, UserId: rows[0].UserId, DateTimeIn: Date.now()};
+
+			        		db.query("INSERT INTO UsersSessions SET ?", session, function(err, result) {
+
+			        			if(err) {
+			        				console.log(err);
+			        				response.send(500, {error:'An error has occured.'});
+			        			} else {
+			        				console.log("Session created: " + key);
+			        				response.send(200, {key: key});
+			        			}
+
+			        		});
+    					}
+    				}
+
+    			});
+    		}
+
+    	}
+
+
+    });
+
 });
 
 /**
@@ -131,7 +178,98 @@ app.post('/space', function (request, response) {
     response.send(200, {space:{}});
 });
 
+app.get('/verify', function (request, response) {
+
+	var key = request.headers['authorization'];
+
+	if(typeof key != 'undefined' && key != null)
+		verifySessionKey(key, function(valid) {
+			response.send(200, {valid:valid});
+		});
+
+});
+
+app.post('/register', function (request, response) {
+
+    var username = request.body.username;
+    var password = request.body.password;
+
+	bcrypt.genSalt(10, function(err, salt) {
+
+	    bcrypt.hash(password, salt, function(err2, hash) {
+	        // Store user/hash/salt
+	        //console.log(password + " -> " + hash + " : " + salt);
+	        var user = {UserName: username, Password: hash, Salt: salt, RoleId: 4, Email:'', FirstName: '', LastName: ''};
+
+	        if(err || err2) {
+				console.log(err);
+				console.log(err2);
+	        	response.send(500, {error:'An error has occured.'});
+
+	        } else {
+
+	        	db.query("INSERT INTO Users SET ?", user, function(err, result) {
+
+		        	if(err) {
+		        		console.log(err);
+		        		response.send(500, {error:'An error has occured.'});
+		        	} else {
+		        		//Generate the session key
+		        		var key = generateSessionKey(username);
+		        		var session = {SessionKey: key, UserId: result.insertId, DateTimeIn: Date.now()};
+
+		        		db.query("INSERT INTO UsersSessions SET ?", session, function(err, result) {
+
+		        			if(err) {
+		        				console.log(err);
+		        				response.send(500, {error:'An error has occured.'});
+		        			} else {
+		        				console.log("Session created: " + key);
+		        				response.send(200, {key: key});
+		        			}
+
+		        		});
+
+		        	}
+
+		        }); // query insert user
+
+	        }
+
+
+	    }); //hash
+
+
+	}); //genSalt
+
+
+}); //app.post
+
 
 app.listen(port, function() {
     console.log("Server listening on port " + port);
 });
+
+
+function generateSessionKey(username) {
+
+	var sha = crypto.createHash('sha1');
+
+	sha.update(username + Date.now() + Math.random());
+	return sha.digest('hex');
+}
+
+function verifySessionKey(key, callback) {
+
+	db.query("SELECT SessionId FROM UsersSessions WHERE SessionKey = ?", key, function(err, rows, fields) {
+
+		if(err || rows.length === 0) {
+			console.log(err);
+			callback(false); 
+		} else {
+			callback(true);
+		}
+
+	});
+
+}
