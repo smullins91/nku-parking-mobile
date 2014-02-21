5c75ee01ebe7f13f788015f29f1a039f2e165436
aryalgaurav@gmail.com <aryalgaurav@gmail.com@72710deb-69ad-d998-ebc1-45e36a55b302>
2014-02-21 20:31:58 +0000

    The tabs are working now. The Map tab has the map view whereas the navigation lot options has the navigation items. There are 4 options in the navigation lot options which I basically copied it from the requirement document. Next, we will make selections from the navigation lot options and move forward from there.

--- AndroidApp/app/build/source/r/debug/com/google/android/gms/R.java
+++ AndroidApp/app/build/source/r/debug/com/google/android/gms/R.java
@@ -27,18 +27,18 @@ public final class R {
 		public static final int zOrderOnTop = 0x7f010068;
 	}
 	public static final class color {
-		public static final int common_action_bar_splitter = 0x7f060003;
-		public static final int common_signin_btn_dark_text_default = 0x7f060004;
-		public static final int common_signin_btn_dark_text_disabled = 0x7f060005;
-		public static final int common_signin_btn_dark_text_focused = 0x7f060006;
-		public static final int common_signin_btn_dark_text_pressed = 0x7f060007;
-		public static final int common_signin_btn_default_background = 0x7f060008;
-		public static final int common_signin_btn_light_text_default = 0x7f060009;
-		public static final int common_signin_btn_light_text_disabled = 0x7f06000a;
-		public static final int common_signin_btn_light_text_focused = 0x7f06000b;
-		public static final int common_signin_btn_light_text_pressed = 0x7f06000c;
-		public static final int common_signin_btn_text_dark = 0x7f06000e;
-		public static final int common_signin_btn_text_light = 0x7f06000f;
+		public static final int common_action_bar_splitter = 0x7f070003;
+		public static final int common_signin_btn_dark_text_default = 0x7f070004;
+		public static final int common_signin_btn_dark_text_disabled = 0x7f070005;
+		public static final int common_signin_btn_dark_text_focused = 0x7f070006;
+		public static final int common_signin_btn_dark_text_pressed = 0x7f070007;
+		public static final int common_signin_btn_default_background = 0x7f070008;
+		public static final int common_signin_btn_light_text_default = 0x7f070009;
+		public static final int common_signin_btn_light_text_disabled = 0x7f07000a;
+		public static final int common_signin_btn_light_text_focused = 0x7f07000b;
+		public static final int common_signin_btn_light_text_pressed = 0x7f07000c;
+		public static final int common_signin_btn_text_dark = 0x7f07000e;
+		public static final int common_signin_btn_text_light = 0x7f07000f;
 	}
 	public static final class drawable {
 		public static final int common_signin_btn_icon_dark = 0x7f020057;
@@ -71,42 +71,42 @@ public final class R {
 		public static final int ic_plusone_tall_off_client = 0x7f020073;
 	}
 	public static final class id {
-		public static final int hybrid = 0x7f07000f;
-		public static final int none = 0x7f070009;
-		public static final int normal = 0x7f070000;
-		public static final int satellite = 0x7f07000d;
-		public static final int terrain = 0x7f07000e;
+		public static final int hybrid = 0x7f08000f;
+		public static final int none = 0x7f080009;
+		public static final int normal = 0x7f080000;
+		public static final int satellite = 0x7f08000d;
+		public static final int terrain = 0x7f08000e;
 	}
 	public static final class integer {
-		public static final int google_play_services_version = 0x7f090001;
+		public static final int google_play_services_version = 0x7f0a0001;
 	}
 	public static final class string {
-		public static final int auth_client_needs_enabling_title = 0x7f0a0012;
-		public static final int auth_client_needs_installation_title = 0x7f0a0013;
-		public static final int auth_client_needs_update_title = 0x7f0a0014;
-		public static final int auth_client_play_services_err_notification_msg = 0x7f0a0015;
-		public static final int auth_client_requested_by_msg = 0x7f0a0016;
-		public static final int auth_client_using_bad_version_title = 0x7f0a0017;
-		public static final int common_google_play_services_enable_button = 0x7f0a0018;
-		public static final int common_google_play_services_enable_text = 0x7f0a0019;
-		public static final int common_google_play_services_enable_title = 0x7f0a001a;
-		public static final int common_google_play_services_install_button = 0x7f0a001b;
-		public static final int common_google_play_services_install_text_phone = 0x7f0a001c;
-		public static final int common_google_play_services_install_text_tablet = 0x7f0a001d;
-		public static final int common_google_play_services_install_title = 0x7f0a001e;
-		public static final int common_google_play_services_invalid_account_text = 0x7f0a001f;
-		public static final int common_google_play_services_invalid_account_title = 0x7f0a0020;
-		public static final int common_google_play_services_network_error_text = 0x7f0a0021;
-		public static final int common_google_play_services_network_error_title = 0x7f0a0022;
-		public static final int common_google_play_services_unknown_issue = 0x7f0a0023;
-		public static final int common_google_play_services_unsupported_date_text = 0x7f0a0024;
-		public static final int common_google_play_services_unsupported_text = 0x7f0a0025;
-		public static final int common_google_play_services_unsupported_title = 0x7f0a0026;
-		public static final int common_google_play_services_update_button = 0x7f0a0027;
-		public static final int common_google_play_services_update_text = 0x7f0a0028;
-		public static final int common_google_play_services_update_title = 0x7f0a0029;
-		public static final int common_signin_button_text = 0x7f0a002a;
-		public static final int common_signin_button_text_long = 0x7f0a002b;
+		public static final int auth_client_needs_enabling_title = 0x7f0b0012;
+		public static final int auth_client_needs_installation_title = 0x7f0b0013;
+		public static final int auth_client_needs_update_title = 0x7f0b0014;
+		public static final int auth_client_play_services_err_notification_msg = 0x7f0b0015;
+		public static final int auth_client_requested_by_msg = 0x7f0b0016;
+		public static final int auth_client_using_bad_version_title = 0x7f0b0017;
+		public static final int common_google_play_services_enable_button = 0x7f0b0018;
+		public static final int common_google_play_services_enable_text = 0x7f0b0019;
+		public static final int common_google_play_services_enable_title = 0x7f0b001a;
+		public static final int common_google_play_services_install_button = 0x7f0b001b;
+		public static final int common_google_play_services_install_text_phone = 0x7f0b001c;
+		public static final int common_google_play_services_install_text_tablet = 0x7f0b001d;
+		public static final int common_google_play_services_install_title = 0x7f0b001e;
+		public static final int common_google_play_services_invalid_account_text = 0x7f0b001f;
+		public static final int common_google_play_services_invalid_account_title = 0x7f0b0020;
+		public static final int common_google_play_services_network_error_text = 0x7f0b0021;
+		public static final int common_google_play_services_network_error_title = 0x7f0b0022;
+		public static final int common_google_play_services_unknown_issue = 0x7f0b0023;
+		public static final int common_google_play_services_unsupported_date_text = 0x7f0b0024;
+		public static final int common_google_play_services_unsupported_text = 0x7f0b0025;
+		public static final int common_google_play_services_unsupported_title = 0x7f0b0026;
+		public static final int common_google_play_services_update_button = 0x7f0b0027;
+		public static final int common_google_play_services_update_text = 0x7f0b0028;
+		public static final int common_google_play_services_update_title = 0x7f0b0029;
+		public static final int common_signin_button_text = 0x7f0b002a;
+		public static final int common_signin_button_text_long = 0x7f0b002b;
 	}
 	public static final class styleable {
 		public static final int[] AdsAttrs = { 0x7f010055, 0x7f010056, 0x7f010057 };
---
+++ AndroidApp/app/src/main/java/com/capstoneproject/app/TabsPagerAdapter.java
@@ -0,0 +1,33 @@
+package com.capstoneproject.app;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class TabsPagerAdapter extends FragmentPagerAdapter
+{
+    public TabsPagerAdapter(FragmentManager fragmentManager)
+    {
+        super(fragmentManager);
+    }
+
+    @Override
+    public Fragment getItem(int index)
+    {
+        switch (index)
+        {
+            case 0:
+                return new MapFragment(); // Fragment activity for the map
+            case 1:
+
+                return new ParkingNavigationFragment(); // Fragment activity for the navigation
+        }
+        return null;
+    }
+
+    @Override
+    public int getCount()
+    {
+        return 2; // get item count - equal to number of tabs
+    }
+}
