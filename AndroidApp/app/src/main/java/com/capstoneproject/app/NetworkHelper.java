4f1f771de97a3ae36a6075822f79c8b7e829635c
mattj24@gmail.com <mattj24@gmail.com@72710deb-69ad-d998-ebc1-45e36a55b302>
2014-03-15 04:13:45 +0000

    Android App now uses SSL to communicate with the API.

--- AndroidApp/app/src/main/java/com/capstoneproject/app/NetworkHelper.java
+++ AndroidApp/app/src/main/java/com/capstoneproject/app/NetworkHelper.java
@@ -10,11 +10,26 @@ import org.apache.http.entity.StringEntity;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.net.Socket;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import org.apache.http.conn.ssl.SSLSocketFactory;
+
 
 public class NetworkHelper {
 
-    public static final String API_URL = "http://ec2-54-200-98-161.us-west-2.compute.amazonaws.com:8080/";
+    public static final String API_URL = "https://ec2-54-200-98-161.us-west-2.compute.amazonaws.com/";
     public static final String API_LOGIN = "login";
     public static final String API_REGISTER = "register";
     public static final String API_VERIFY = "verify";
@@ -64,7 +79,52 @@ public class NetworkHelper {
         client.addHeader(HEADER_AUTH, key);
         client.setTimeout(10 * 1000);
 
+        try {
+            KeyStore trustStore = KeyStore.getInstance(KeyStore.getDefaultType());
+            trustStore.load(null, null);
+            CustomSSL sf = new CustomSSL(trustStore);
+            sf.setHostnameVerifier(CustomSSL.ALLOW_ALL_HOSTNAME_VERIFIER);
+            client.setSSLSocketFactory(sf);
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+
         return client;
+    }
+
+
+    private static class CustomSSL extends SSLSocketFactory {
+
+        SSLContext sslContext = SSLContext.getInstance("TLS");
+
+        public CustomSSL(KeyStore truststore) throws NoSuchAlgorithmException, KeyManagementException, KeyStoreException, UnrecoverableKeyException {
+
+            super(truststore);
+
+            TrustManager tm = new X509TrustManager() {
+                public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+                }
+
+                public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+                }
+
+                public X509Certificate[] getAcceptedIssuers() {
+                    return null;
+                }
+            };
+
+            sslContext.init(null, new TrustManager[] { tm }, null);
+        }
+
+        @Override
+        public Socket createSocket(Socket socket, String host, int port, boolean autoClose) throws IOException {
+            return sslContext.getSocketFactory().createSocket(socket, host, port, autoClose);
+        }
+
+        @Override
+        public Socket createSocket() throws IOException {
+            return sslContext.getSocketFactory().createSocket();
+        }
     }
 
 }
