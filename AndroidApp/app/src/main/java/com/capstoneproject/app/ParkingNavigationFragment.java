9bc76ab2dfe3be08cb84501beac50c57a5a62cc3
mattj24@gmail.com <mattj24@gmail.com@72710deb-69ad-d998-ebc1-45e36a55b302>
2014-03-21 04:22:48 +0000

    Added a card UI library to the Android app

--- AndroidApp/app/src/main/java/com/capstoneproject/app/ParkingNavigationFragment.java
+++ AndroidApp/app/src/main/java/com/capstoneproject/app/ParkingNavigationFragment.java
@@ -6,11 +6,51 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class ParkingNavigationFragment extends Fragment
-{
+import com.nhaarman.listviewanimations.swinginadapters.prepared.ScaleInAnimationAdapter;
+import com.nhaarman.listviewanimations.swinginadapters.prepared.SwingBottomInAnimationAdapter;
+
+import java.util.ArrayList;
+
+import it.gmariotti.cardslib.library.internal.Card;
+import it.gmariotti.cardslib.library.internal.CardArrayAdapter;
+import it.gmariotti.cardslib.library.internal.CardHeader;
+import it.gmariotti.cardslib.library.view.CardListView;
+import it.gmariotti.cardslib.library.view.CardView;
+
+public class ParkingNavigationFragment extends Fragment {
+
+    private CardListView mCardView;
+    private CardArrayAdapter mCardAdapter;
+    private ArrayList<Card> mCards;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
-        return inflater.inflate(R.layout.fragment_navigation, container, false);
+        View v = inflater.inflate(R.layout.fragment_lots, container, false);
+
+        CardListView mCardView = (CardListView) v.findViewById(R.id.lot_list);
+        mCards = new ArrayList<Card>();
+        CardArrayAdapter mCardAdapter = new CardArrayAdapter(getActivity(), mCards);
+        ScaleInAnimationAdapter animationAdapter = new ScaleInAnimationAdapter(mCardAdapter);
+        animationAdapter.setAbsListView(mCardView);
+
+        for(int i = 1; i <= 3; i++) {
+
+            Card card = new Card(getActivity());
+            CardHeader cardHeader = new CardHeader(getActivity());
+
+            cardHeader.setTitle("Parking Lot " + i);
+
+            card.addCardHeader(cardHeader);
+            mCardAdapter.add(card);
+
+        }
+
+        mCardView.setExternalAdapter(animationAdapter, mCardAdapter);
+
+        //return inflater.inflate(R.layout.fragment_navigation, container, false);
+        return v;
     }
+
+
 }
