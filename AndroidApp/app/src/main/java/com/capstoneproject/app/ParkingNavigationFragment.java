b67fa22303760902debc71f1e36f9986f2be7f48
aryalgaurav@gmail.com <aryalgaurav@gmail.com@72710deb-69ad-d998-ebc1-45e36a55b302>
2014-03-24 02:33:38 +0000

    Added the names of the cards. When you click on the cards, you get 3 menu options.

--- AndroidApp/app/src/main/java/com/capstoneproject/app/ParkingNavigationFragment.java
+++ AndroidApp/app/src/main/java/com/capstoneproject/app/ParkingNavigationFragment.java
@@ -1,10 +1,16 @@
 package com.capstoneproject.app;
 
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.nhaarman.listviewanimations.swinginadapters.prepared.ScaleInAnimationAdapter;
 import com.nhaarman.listviewanimations.swinginadapters.prepared.SwingBottomInAnimationAdapter;
@@ -13,7 +19,9 @@ import java.util.ArrayList;
 
 import it.gmariotti.cardslib.library.internal.Card;
 import it.gmariotti.cardslib.library.internal.CardArrayAdapter;
+import it.gmariotti.cardslib.library.internal.CardExpand;
 import it.gmariotti.cardslib.library.internal.CardHeader;
+import it.gmariotti.cardslib.library.internal.base.BaseCard;
 import it.gmariotti.cardslib.library.view.CardListView;
 import it.gmariotti.cardslib.library.view.CardView;
 
@@ -34,23 +42,63 @@ public class ParkingNavigationFragment extends Fragment {
         ScaleInAnimationAdapter animationAdapter = new ScaleInAnimationAdapter(mCardAdapter);
         animationAdapter.setAbsListView(mCardView);
 
-        for(int i = 1; i <= 3; i++) {
+        //initializeCardInformation();
 
+        String[] parkingLotNames = {"Student Parking Lot 1","Student Parking Lot 2","Faculty Parking Lot 1","Faculty Parking Lot 2","Visitor Parking Lot 1"};
+
+        for(int i = 0; i < parkingLotNames.length; i++)
+        {
             Card card = new Card(getActivity());
             CardHeader cardHeader = new CardHeader(getActivity());
 
-            cardHeader.setTitle("Parking Lot " + i);
+            cardHeader.setTitle(parkingLotNames[i]);
+            cardHeader.setPopupMenu(R.menu.parking_status, new CardHeader.OnClickCardHeaderPopupMenuListener()
+            {
+                @Override
+                public void onMenuItemClick(BaseCard baseCard, MenuItem item) {
+                    //Toast.makeText(getActivity(), "Clicked on " + item.getTitle(), Toast.LENGTH_SHORT).show();
+                    onMenuOptionSelected(item);
+                }
+            });
 
             card.addCardHeader(cardHeader);
             mCardAdapter.add(card);
-
         }
 
         mCardView.setExternalAdapter(animationAdapter, mCardAdapter);
-
         //return inflater.inflate(R.layout.fragment_navigation, container, false);
         return v;
     }
 
+    public boolean onMenuOptionSelected(MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+            case R.id.Availability:
+                String getValueFromDatabase = "Available";
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                builder.setTitle("Lot Status").setMessage("The lot status is : " + getValueFromDatabase);
+                builder.setNeutralButton("OK", new DialogInterface.OnClickListener()
+                {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i)
+                    {
 
+                    }
+                });
+                builder.setIcon(R.drawable.ic_launcher).show();
+                break;
+
+            case R.id.DirectionToLot:
+                Toast.makeText(getActivity(), "Item 2 has been selected", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.ReserveASpot:
+                Toast.makeText(getActivity(), "Item 3 has been selected", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
+        //Toast.makeText(getActivity(), "Clicked on " + item.getTitle(), Toast.LENGTH_SHORT).show();
+        return true;
+    }
 }
