53e6bd3be4547d3c5461e306a5a1f0188133b6d4
aryalgaurav@gmail.com <aryalgaurav@gmail.com@72710deb-69ad-d998-ebc1-45e36a55b302>
2014-03-29 21:54:32 +0000

    Added spaces between parking lots. Very minor commit, nothing much added.

--- trunk/AndroidApp/app/src/main/java/com/capstoneproject/app/ParkingSpaces.java
+++ trunk/AndroidApp/app/src/main/java/com/capstoneproject/app/ParkingSpaces.java
@@ -4,8 +4,10 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.GridLayout;
@@ -13,7 +15,7 @@ import android.widget.Toast;
 
 public class ParkingSpaces extends Activity implements View.OnClickListener
 {
-    private static final int NUMBER_OF_ROWS = 5;
+    private static final int NUMBER_OF_ROWS = 11;
     private static final int NUMBER_OF_COLUMNS = 5;
 
     @Override
@@ -22,8 +24,7 @@ public class ParkingSpaces extends Activity implements View.OnClickListener
         super.onCreate(savedInstanceState);
         setContentView(R.layout.parking_spaces_main);
 
-        InputMethodManager im = (InputMethodManager) this.getApplicationContext().getSystemService(Context.INPUT_METHOD_SERVICE);
-        im.hideSoftInputFromWindow(getWindow().getDecorView().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN); //hide the keyboard
 
         initializeParkingSpaces();
     }
@@ -31,18 +32,48 @@ public class ParkingSpaces extends Activity implements View.OnClickListener
     private void initializeParkingSpaces()
     {
         GridLayout buttonContainer = (GridLayout) findViewById(R.id.parking_spaces);
-        buttonContainer.setRowCount(5);
+        buttonContainer.setColumnCount(NUMBER_OF_COLUMNS);
 
         for (int rows = 0; rows < NUMBER_OF_ROWS; rows++)
         {
             for (int columns = 0; columns < NUMBER_OF_COLUMNS; columns++)
             {
                 Button button = new Button(getBaseContext());
+
                 int key = (Integer.parseInt("" + rows + columns));
                 button.setId(key);
-                button.setText("" + rows + "," + columns);
+                button.setText(rows + "," + columns);
                 button.setOnClickListener(this); //calls the onclick method of the interface
-                buttonContainer.addView(button);
+                button.setTextColor(Color.BLACK);
+
+                GridLayout.LayoutParams params = new GridLayout.LayoutParams();
+                //params.height = 40;
+                //params.width = 70;
+
+                if ((columns != 0) && (columns % 2 != 0))
+                {
+                    params.rightMargin = 80;
+                }
+                else
+                {
+                    params.rightMargin = 6;
+                }
+
+                if (rows == columns) //hard coded, get the value from the database.
+                {
+                    //button.setTextColor(Color.RED);
+                    button.setBackgroundColor(Color.RED);
+
+                }
+                else if (rows + 2 == columns)  //hard coded, get the value from the database.
+                {
+                    button.setBackgroundColor(Color.YELLOW);  //hard coded, get the value from the database.
+                }
+                else
+                {
+                    button.setBackgroundColor(Color.GREEN);  //hard coded, get the value from the database.
+                }
+                buttonContainer.addView(button, params);
             }
         }
     }
@@ -55,7 +86,7 @@ public class ParkingSpaces extends Activity implements View.OnClickListener
 
 
 
-        if (buttonText.equals("1,0") || buttonText.equals("1,1") || buttonText.equals("1,2") || buttonText.equals("1,3") || buttonText.equals("1,4"))
+        if (buttonText.equals("0,0") || buttonText.equals("1,1") || buttonText.equals("2,2") || buttonText.equals("3,3") || buttonText.equals("4,4"))
         {
             String getValueFromDatabase = "Reserved";
             AlertDialog.Builder builder = new AlertDialog.Builder(this);
@@ -75,10 +106,10 @@ public class ParkingSpaces extends Activity implements View.OnClickListener
                     .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
                         public void onClick(DialogInterface dialog, int which)
                         {
-                            Toast.makeText(getBaseContext(), "You have reserved a spot. Your reservation is valid for 1 hour. Thank you!", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(getBaseContext(), "You have reserved a spot. Your reservation is valid for 1 hour. Thank you!", Toast.LENGTH_LONG).show();
                         }
 
-                    }).setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+            }).setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
                         public void onClick(DialogInterface dialog, int which)
                         {
                             //No selected. So, do nothing, go back to the activity
