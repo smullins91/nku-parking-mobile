07c8b1b0ffaf26bcd8fcf6296fbed29c830f4951
chris45215@gmail.com <chris45215@gmail.com@72710deb-69ad-d998-ebc1-45e36a55b302>
2014-04-09 03:06:16 +0000

    Complete overhaul to the GUI in the Management page. The behind-the-scenes code still needs to be added (ESPECIALLY for getting the lot coordinates from the DB and sending them to the Google Maps API - that will be tricky, but doable).

--- WebApp/ParkingManagement/ParkingManagement/WebContent/ParkingManagement/ParkingManagement.aspx.cs
+++ WebApp/ParkingManagement/ParkingManagement/WebContent/ParkingManagement/ParkingManagement.aspx.cs
@@ -14,6 +14,8 @@ namespace ParkingManagement.WebContent.ParkingManagement
 {
     public partial class ParkingManagement : System.Web.UI.Page
     {
+        static string serverAddress = @"http://ec2-54-200-98-161.us-west-2.compute.amazonaws.com:8080";
+
         //YOU MAY NEED TO DOWNLOAD GoogleMapsAPI from NuGet to make this work
 
         string[] gotNames;
@@ -23,26 +25,38 @@ namespace ParkingManagement.WebContent.ParkingManagement
             /* load the parking lot names from the database, and then populate the 
              * dropdown list with those names.
              */
+
+
+
+      //      DropDownList4.Text = "Row";
+      //      DropDownList5.Text = "Column";
+
+            /*
             gotNames = Shared.getLotNames();
             for (int i = 0; i < gotNames.Length; i++)
             {
                 DropDownList1.Items.Add(gotNames[i].ToString());
             }
-
-            //THIS IS A HACK TO MAKE THE MAP PANEL LARGE ENOUGH, so the rest of the page formats correctly too.
-           // Panel1.Height = 460;
-             
+             */
         }
 
-        
+        public enum ViewSelected
+        {
+            NotSet = -1,
+            EditLot = 0,
+            AddLot = 1
+        }
 
 
         protected void Button1_Click(object sender, EventArgs e)
         {
-
+            MultiViewManage.ActiveViewIndex = (int)ViewSelected.EditLot;
+        }
+        protected void Button6_Click(object sender, EventArgs e)
+        {
+            MultiViewManage.ActiveViewIndex = (int)ViewSelected.AddLot;
         }
 
-       
 
         protected void Button5_Click(object sender, EventArgs e)
         {
@@ -54,8 +68,84 @@ namespace ParkingManagement.WebContent.ParkingManagement
 
         }
 
-        
+        public static Rootobject getParkingLots()
+        {
+            HttpWebRequest req = WebRequest.Create(serverAddress + "/lots") as HttpWebRequest;
+            req.ContentType = "application/json";
+            req.Method = WebRequestMethods.Http.Get;
+            req.Headers.Add("Authorization", "3addbbc3d6a464eba3f57993411144158b0d312c");
+
+            string result;
+            Rootobject resultObject;
+            // AllLots resultObject;
+            using (HttpWebResponse resp = req.GetResponse() as HttpWebResponse)
+            {
+                StreamReader reader = new StreamReader(resp.GetResponseStream());
+                result = reader.ReadToEnd();
+                result.Trim();
+                //   string stringResult = result.ToString();
+                string stringResult = result.Substring(1, result.Length - 2);   //We are geting square brackets that make things complicated.
+                //When the database has more than 1 lot, it should fix itself.
+
+                resultObject = JsonConvert.DeserializeObject<Rootobject>(stringResult);
+                // resultObject = JsonConvert.DeserializeObject<AllLots>(stringResult);
+            }
+            return resultObject;
+        }
+
+
+
+        public class Rootobject
+        {
+            public Class1[] Property1 { get; set; }
+        }
+
+        public class Class1
+        {
+            public int id { get; set; }
+            public string lotNumber { get; set; }
+            public int type { get; set; }
+            public int active { get; set; }
+            public int rows { get; set; }
+            public int columns { get; set; }
+            public int available { get; set; }
+            public Point[] points { get; set; }
+        }
+
+        public class Point
+        {
+            public float lat { get; set; }
+            public float lng { get; set; }
+        }
+
+        protected void Button9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
 
 
     }
+
+
+
+
+
 }
